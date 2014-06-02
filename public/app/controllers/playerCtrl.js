83d6324dcbcd4a1765a9d2ac89d5177d3173f57a
jornare <jornare@hatlelid.net>
2014-06-02 22:55:59 +0200

    redirect a player with no handle to the main menu

--- public/app/controllers/playerCtrl.js
+++ public/app/controllers/playerCtrl.js
@@ -2,34 +2,39 @@
 
 
     angular.module('app')
-        .controller('PlayerCtrl', ['$scope', '$game', '$socket', function($scope, $game, $socket){
-            $scope.$game = $game;
-            $scope.fly = $game.fly;
-            $scope.color = 'gray';
-            $scope.me = $game.me;
-            $scope.$watch('$game.me', function (me) {
-                if (me) {
-                    $scope.me = me;
-                    //$game.play();
-                    $scope.color = me.color;
-                    $scope.$watch('me.color', function (color) {
-                        if (color) {
-                            $scope.color = me.color;
+        .controller('PlayerCtrl',
+            ['$scope', '$game', '$socket', '$location',
+            function ($scope, $game, $socket, $location) {
+                $scope.$game = $game;
+                $scope.fly = $game.fly;
+                $scope.color = 'gray';
+                $scope.me = $game.me;
+                $scope.$watch('$game.me', function (me) {
+                    if (me) {
+                        $scope.me = me;
+                        if (!me.handle) {
+                            $location.url('/');
+                            return;
                         }
-                    });
-                    $scope.$watch('me.playing', function (playing) {
-                        if (!playing) {
-                            $game.play();
-                        }
+                        $scope.color = me.color;
+                        $scope.$watch('me.color', function (color) {
+                            if (color) {
+                                $scope.color = me.color;
+                            }
+                        });
+                        $scope.$watch('me.playing', function (playing) {
+                            if (!playing) {
+                                $game.play();
+                            }
 
-                    });
-                }
-            });
+                        });
+                    }
+                });
 
 
-            $scope.$on('$destroy', function () {
-                $game.stopPlay();
-            });
+                $scope.$on('$destroy', function () {
+                    $game.stopPlay();
+                });
 
         }]);
 

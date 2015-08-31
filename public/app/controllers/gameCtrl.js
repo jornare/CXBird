24aefcd0b294b54f2ebe0d8aa0f44a2a3f19199c
jornare <jornare@hatlelid.net>
2015-08-31 22:10:20 +0200

    feature: top score on game page

--- public/app/controllers/gameCtrl.js
+++ public/app/controllers/gameCtrl.js
@@ -6,8 +6,11 @@
             $scope.game = $game;
             $game.watchGame();
 
-            $scope.players = $game.players.playing;
-
+			$scope.players = $game.players.playing;
+			$scope.highscores = $game.highscores;
+			setInterval(function () {
+				console.log($scope.highscores);
+			}, 1000);
             //$socket.on('updatePlayer', function (data) {
             //    $scope.players = $game.players.playing;
             //});

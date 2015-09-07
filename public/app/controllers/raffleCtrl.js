4dc9725cc468e5986981b392894dc94005f2e049
jornare <jornare@hatlelid.net>
2015-09-07 22:57:18 +0200

    feature: raffle re-draw

--- public/app/controllers/raffleCtrl.js
+++ public/app/controllers/raffleCtrl.js
@@ -22,12 +22,11 @@
         .controller('RaffleCtrl',
             ['$scope', '$game', '$timeout',
                 function ($scope, $game, $timeout) {
-				    var isRaffleRunning = false;
+                    $scope.isRaffleRunning = false;
                     $scope.p = $game.players.all;
-                    $scope.$watch('p.length', function () {
-					    if(isRaffleRunning) {
-						    return;
-					    }
+                    $scope.winner = null;
+            
+                    function createTickets() {
                         var tickets = [], extra = [], i, j, p = $game.players.all;
                         for (i = 0; i < p.length; i++) {
                             if (!p[i].handle) {
@@ -41,15 +40,30 @@
                             tickets.push({ handle: p[i].handle });
                         }
                         $scope.tickets = tickets.shuffle();
-                    });
+                    }
 
-                    $scope.raffle = function () {
-					    if(isRaffleRunning) {
+                    $scope.$watch('p.length', function () {
+					    if($scope.isRaffleRunning) {
 						    return;
 					    }
-                        var i, winner = Math.floor(Math.random() * $scope.tickets.length), t = $scope.tickets;
-                        isRaffleRunning = true;
-						raffle(winner);
+                        createTickets();
+                    });
+            
+                    $scope.reRaffle = function (){
+                        if ($scope.isRaffleRunning) {
+                            return;
+                        }
+                        $scope.winner = null;
+                        createTickets();
+                    }
+
+                    $scope.raffle = function () {
+					    if($scope.isRaffleRunning) {
+						    return;
+                        }
+                        var winner = Math.floor(Math.random() * $scope.tickets.length);
+                        $scope.isRaffleRunning = true;
+                        raffle(winner);
                     };
 
                     function raffle(winner) {
@@ -65,6 +79,7 @@
                         } else {
                             $timeout(function () {
                                 $scope.winner = $scope.tickets[0].handle;
+                                $scope.isRaffleRunning = false;
                             }, 2000);
                             //$scope.tickets.pop();
                            // alert('Winner! is ' + $scope.tickets[0].handle);
